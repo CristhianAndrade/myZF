5478ec10a7122f437ffe692c6dcb159e4ba7d098
Cristhian Andrade <cristhian@soulmkt.com.br>
2015-07-28 19:29:27 -0300

    ajustes frontend base

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,6 +2,10 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+	public function _initSomeservice()
+	{
+	    $someservice = 'application/layouts/frontend/';
+	    Zend_Registry::set('path_include', $someservice); 
+	}
 
 }
